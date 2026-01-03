8aca23a3aa26bb8c61c59a0a05cd2cff39504a88
paras <parasrana1728@gmail.com>
2026-01-03 18:07:51 +0530

    added insertFirst and display methods

--- learnJava/src/com/paras/LL.java
+++ learnJava/src/com/paras/LL.java
@@ -10,6 +10,25 @@ public class LL {
         this.size = 0;
     }
 
+    public void insertFirst(int val) {
+        Node node = new Node(val);
+        node.next = head;
+        head = node;
+        if (tail == null) {
+            tail = head;
+        }
+        size++;
+    }
+
+    public void display() {
+        Node temp = head;
+        while (temp != null) {
+            System.out.print(temp.val + " -> ");
+            temp = temp.next;
+        }
+        System.out.print("NULL");
+        System.out.println();
+    }
     private class Node {
         private int val;
         private Node next;
--- learnJava/src/com/paras/Main.java
+++ learnJava/src/com/paras/Main.java
@@ -2,6 +2,10 @@ package com.paras;
 
 public class Main {
     static void main() {
-
+        LL list = new LL();
+        list.insertFirst(5);
+        list.insertFirst(42);
+        list.insertFirst(3);
+        list.display();
     }
 }
