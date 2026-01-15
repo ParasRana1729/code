07529ec08b517511d465a29699425d4f18db61d9
paras <parasrana1728@gmail.com>
2026-01-15 10:20:44 +0530

    feat: Add user filtering logic to display female names

---
+++ webDev/warmup/js/index.js
@@ -0,0 +1,18 @@
+// const randomeNumber = [2, 4, 3, 5, 8, 1, 6, 7];
+
+// for (let i = 0; i < randomeNumber.length; i++) {
+//     if (randomeNumber[i] % 2 === 0) {
+//         console.log(randomeNumber[i]);
+//     }
+// }
+
+const allUsers = [
+    { firstName: "John", gender: "Male", age: 25 },
+    { firstName: "Jane", gender: "Female", age: 17 }
+]
+
+for (let i = 0; i < allUsers.length; i++) {
+    if (allUsers[i].gender == "Female") {
+        console.log(allUsers[i].firstName);
+    }
+}
