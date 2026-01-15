6688b0f4b116ea6ee32108df15c310cd149bbde3
paras <parasrana1728@gmail.com>
2026-01-15 10:48:34 +0530

    refactor: Remove commented-out user filtering code and maintain calculation functions

--- webDev/warmup/js/index.js
+++ webDev/warmup/js/index.js
@@ -6,13 +6,30 @@
 //     }
 // }
 
-const allUsers = [
-    { firstName: "John", gender: "Male", age: 25 },
-    { firstName: "Jane", gender: "Female", age: 17 }
-]
+// const allUsers = [
+//     { firstName: "John", gender: "Male", age: 25 },
+//     { firstName: "Jane", gender: "Female", age: 17 }
+// ]
 
-for (let i = 0; i < allUsers.length; i++) {
-    if (allUsers[i].gender == "Female") {
-        console.log(allUsers[i].firstName);
-    }
+// for (let i = 0; i < allUsers.length; i++) {
+//     if (allUsers[i].gender == "Female") {
+//         console.log(allUsers[i].firstName);
+//     }
+// }
+
+// callback
+
+function calculate(num1, num2, operation) {
+    return operation(num1, num2);
 }
+
+function add(a, b) {
+    return a + b;
+}
+
+function multiply(a, b) {
+    return a * b;
+}
+
+console.log(calculate(5, 3, add));
+console.log(calculate(5, 3, multiply));
