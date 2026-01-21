8e23354153d06f2ead16a09d71db30974afabbab
paras <parasrana1728@gmail.com>
2026-01-21 12:59:21 +0530

    feat: Add type checking and typecasting example in test.py

---
+++ python/week-1/test.py
@@ -0,0 +1,8 @@
+age = 25
+print(isinstance(age, int)) # to check if age is of type int
+print(type(age))
+
+age = "25"
+print(isinstance(age, int))
+age = int(age) # typecasting
+print(isinstance(age, int))
