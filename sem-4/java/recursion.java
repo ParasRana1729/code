38f8034e33daf07fb0f797bea2d8bcd379d9edfe
paras <parasrana1728@gmail.com>
2026-01-22 16:53:47 +0530

    refactor: Clean up recursion methods and remove unused print function

--- sem-4/java/recursion.java
+++ sem-4/java/recursion.java
@@ -2,19 +2,17 @@ public class recursion {
 
     static int fact(int n) {
         if (n == 0) return 1;
-        return n * fact(n-1);
+        return n * fact(n - 1);
     }
 
-    static void printn(int n) {
-        if (n == 0) {
-            return;
-        }
-        System.out.println(n);
-        printn(n-1);
+    static int fibo(int n) {
+        if (n == 0) return 0;
+        if (n == 1) return 1;
+        return fibo(n - 1) + fibo(n - 2);
     }
 
     public static void main(String args[]) {
-        // printn(10);
-        System.out.println(fact(5)); 
+        System.out.println(fact(5));
+        System.out.println(fibo(10));
     }
 }
