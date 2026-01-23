639f90aae9bf47bbdb401e2eafbc904a4708380d
paras <parasrana1728@gmail.com>
2026-01-23 08:54:56 +0530

    feat: Initialize backend project with Bun, add basic structure and configuration files

--- sem-4/java/recursion.java
+++ sem-4/java/recursion.java
@@ -1,14 +1,11 @@
 public class recursion {
 
     static int fact(int n) {
-        if (n == 0) return 1;
-        return n * fact(n - 1);
+        return n == 0 ? 1 : n * fact(n - 1);
     }
 
     static int fibo(int n) {
-        if (n == 0) return 0;
-        if (n == 1) return 1;
-        return fibo(n - 1) + fibo(n - 2);
+        return n == 0 ? 0 : n == 1 ? 1 : fibo(n-1) + fibo(n-2); 
     }
 
     public static void main(String args[]) {
