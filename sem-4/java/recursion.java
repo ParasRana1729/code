1c146e7dc8c892d4d06d49ab0aaafbcd19da26ae
paras <parasrana1728@gmail.com>
2026-01-28 09:30:49 +0530

    feat: Add power function to recursion class and update main method to demonstrate its usage; remove redundant checks in anagram function

--- sem-4/java/recursion.java
+++ sem-4/java/recursion.java
@@ -8,8 +8,13 @@ public class recursion {
         return n == 0 ? 0 : n == 1 ? 1 : fibo(n-1) + fibo(n-2); 
     }
 
+    static int power(int base, int exponent) {
+        return exponent == 0 ? 1 : base * (power(base, exponent - 1));
+    }
+
     public static void main(String args[]) {
         System.out.println(fact(5));
         System.out.println(fibo(10));
+        System.out.println(power(2, 10));
     }
 }
