960c6756be3c223a8a357ff49297c6000e304209
paras <parasrana1728@gmail.com>
2026-01-29 20:32:19 +0530

    random changes

--- sem-4/backend/index.js
+++ sem-4/backend/index.js
@@ -1,29 +1,33 @@
-function add(a, b) {
-    return a + b
-}
-setTimeout(
-    () => {
-        console.log("After 2 seconds")
-    }
-, 2000)
-console.log(add(2, 5))
+const fs = require('fs');
+console.log("1");
+const result = fs.readFileSync("file.txt", "utf-8"); 
+console.log(result); // Synchronous way to read a file
+fs.readFile("file.txt", "utf-8", function(err, data){
+    console.log(data); // Asynchronous way to read a file
+});
+console.log("2");
+fs.writeFile("file.txt", "Asynchronous way to write a file", function(err){
+    console.log("File written successfully");
+});
+fs.appendFile("file.txt", "Appending to the file", function(err){
+    console.log("File appended successfully");
+});
+fs.rename("file.txt", "file2.txt", function(err){
+    console.log("File renamed successfully");
+});
+fs.rename("file2.txt", "file.txt", function(err){
+    console.log("File renamed successfully");
+});
 
-function fetchData(callback) {
-    setTimeout(
-        () => {
-            const data = {
-                name : "Paras",
-                class : "CSE"
-            }
-            callback(data);
-        }
-    , 3000)
-}
-
-fetchData(
-    (data) => {
-        console.log(data)
-    } 
-)
-
-console.log("After Callback")
+const os = require('os');
+console.log(os.arch());
+console.log(os.platform());
+console.log(os.homedir());
+console.log(os.hostname());
+console.log(os.tmpdir());
+console.log(os.type());
+console.log(os.version());
+console.log(os.release());
+console.log(os.uptime());
+console.log(os.loadavg());
+console.log(os.totalmem());
---
+++ sem-4/java/tower.java
@@ -0,0 +1,15 @@
+public class tower {
+   
+    public static void tower(int n, String src, String helper, String dest) {
+            if (n == 0) {
+                return;
+            }
+            tower(n-1, src, dest, helper);
+            System.out.println("move disk " + n + " from " + src + " to " + dest);
+            tower(n-1, helper, src, dest);
+        }
+    public static void main(String[] args) {
+        int n = 3;
+        tower(n, "Source", "Helper", "Destination");
+    }
+}
