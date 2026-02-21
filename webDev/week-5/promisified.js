f05482ec3325890b7b21ef85c09e8b8e715c4d35
paras <parasrana1728@gmail.com>
2026-02-21 11:46:05 +0530

    added some promisified functions

---
+++ webDev/week-5/promisified.js
@@ -0,0 +1,45 @@
+// Promise is a javascript object that represents the eventual completion (or failure) of an asynchronous operation and its resulting value.
+
+// Promise has 3 states:
+// 1. Pending: The initial state of a promise.
+// 2. Fulfilled: The promise is resolved successfully.
+// 3. Rejected: The promise is rejected.
+
+// Promise has 2 methods:
+// 1. then: The method to handle the resolved value of the promise.
+// 2. catch: The method to handle the rejected value of the promise.
+
+// ReadFile promisified:
+const fs = require("fs");
+
+function readFilePromisified(filePath, encoding){
+    return new Promise((resolve, reject) => {
+        fs.readFile(filePath, encoding, (err, data) => {
+            if (err) {
+                reject(err);
+            }
+            else {
+                resolve(data);
+            }
+        });
+    });
+}
+
+readFilePromisified("./a.txt", "utf-8")
+    .then(data => {
+        console.log(data);
+    })
+    .catch(err => {
+        console.log(err);
+    });
+
+// setTimeout Promisified
+
+function setTimeoutPromisified(ms) {
+    return new Promise((resolve) => {
+        setTimeout(resolve, ms);
+    })
+}
+
+setTimeoutPromisified(1000)
+    .then(() => console.log("setTimeout Promisified"));
